70a742f00871625f8f63cec48bb7794450c9dc14
jeongyun914 <j.yun@sookmyung.ac.kr>
2026-04-04 20:17:05 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# fix errors and run inference
---
+++ main.py
@@ -0,0 +1,24 @@
+
+import torch
+from PIL import Image
+import torchvision.transforms as transforms
+
+class Net(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc = torch.nn.Linear(28*28, 10)
+    def forward(self,x): return self.fc(x.view(-1,28*28))
+
+model = Net()
+model.load_state_dict(torch.load('mnist.pth'))
+model.eval()
+
+img = Image.open('sample.png').convert('L')
+transform = transforms.ToTensor()
+x = transform(img)
+
+with torch.no_grad():
+    out = model(x)
+    pred = out.argmax(1)
+
+print('prediction:', pred.item())
